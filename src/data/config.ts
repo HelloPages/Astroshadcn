9b3cf1b11866694ed62bad08511deb83612c891d
HelloPages <201108396+HelloPages@users.noreply.github.com>
2025-06-05 15:04:04 +0800

    Update config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -2,7 +2,7 @@ export const config = {
   siteTitle: 'John Doe Portfolio',
   description: 'Hi, I am John Doe, a passionate web developer and data analyst. I thrive on solving problems and building innovative solutions with modern technology.',
   author: 'John Doe',
-  contactEmail: 'johndoe@example.com',
+  contactEmail: 'hello@domainname.plus',
   keywords: 'portfolio, business intelligence, data analysis, Power BI Reports, Software Engineer, Web Developer',
   favicon: '/favicon.svg',
   social: {
