23bee83911901dcb571eeffe5f166a60bd7b6771
HelloPages <201108396+HelloPages@users.noreply.github.com>
2025-06-05 15:07:35 +0800

    Update heroData.ts

--- src/data/heroData.ts
+++ src/data/heroData.ts
@@ -5,21 +5,12 @@ export const heroData = {
   profilePicture: 'https://s2.loli.net/2025/04/02/InhoWUFepGxbcPs.png',
   skills: [
     'JavaScript',
-    'React',
-    'Node.js',
-    'SQL',
-    'Python',
-    'Power BI',
-    'DAX',
-    'ETL',
-    'Data Modeling',
-    'API Development',
     'Cloud Computing',
     'Machine Learning'
   ],
   contact: {
-    github: 'https://github.com/johndoe',
-    linkedin: 'https://www.linkedin.com/in/johndoe/',
-    email: 'johndoe@example.com'
+    github: 'https://github.com/username',
+    linkedin: 'https://www.linkedin.com/in/username/',
+    email: 'hello@domainname.plus'
   }
 };
